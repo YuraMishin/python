8115009e4c96f8d7ae35e2510a5210a073766a67
Mishin Yura <mishin.inbox@gmail.com>
2021-04-12 08:33:32 +0300

    Implemented JPG-PNG converter. Closes #2

---
+++ jpgpngconverter/main.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+from PIL import Image
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(os.path.join(image_folder, filename))
+    clean_name = os.path.splitext(filename)[0]
+    img.save(os.path.join(output_folder, f'{clean_name}.png'), 'png')
+    print('done!')
