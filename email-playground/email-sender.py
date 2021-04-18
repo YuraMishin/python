e418d0ff055db6493331cbbf3a3005f6d9b9c81f
Mishin Yura <mishin.inbox@gmail.com>
2021-04-18 11:29:03 +0300

    Sent HTML email. Closes #7

--- email-playground/email-sender.py
+++ email-playground/email-sender.py
@@ -1,12 +1,16 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Yura Mishin'
 email['to'] = 'user@mail.ru'
 email['subject'] = 'Hello Email!'
 
-email.set_content('Hello dude!')
+email.set_content(html.substitute({'name' : 'Smart_User'}), 'html')
 
 with smtplib.SMTP(host='smtp.mailtrap.io', port=2525) as smtp:
     smtp.ehlo()
